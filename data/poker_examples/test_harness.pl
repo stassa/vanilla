2349cc049ea8e5d4e8008c4d500a503deeefbbcf
YeGoblynQueenne <ep2216@ic.ac.uk>
2025-05-06 13:03:29 +0100

    Refactored generation of examples from language spect terms in Poker. * New predicates generate_initial/[3,6] take the symbol/arity of atoms   to be generated as an argument instead of taking it from the hidden   internal_symbol_/2 predicate that's just dumb. OK, I'm sorry for   calling the predicate dumb, it's me who's dumb but this situation   can't go on. generate_initial/[2,5] are for now deprecated until a   full replacement operation.

--- data/poker_examples/test_harness.pl
+++ data/poker_examples/test_harness.pl
@@ -5,9 +5,11 @@
                       ,experiments/7
                       ,experiment/6
                       ,generate_initial/2
+                      ,generate_initial/3
                       ,generate_initial/5
-                      ,count_initial/2
-                      ,count_initial/5
+                      ,generate_initial/6
+                      ,count_initial/3
+                      ,count_initial/6
                       ,test_labelling/4
                       ,test_program/5
                       %,test_draw/11
@@ -88,8 +90,9 @@ filtering_results(LU,Rs,[Ms_l,SEs_l,Ms_p,SEs_p]):-
               ,[LU,Ms_p,SEs_p]).
 
 
-%!      experiment_filtering(+T,+Ls,+Us,+LPos,+LNeg,+UPos,+UNeg,+LRes,+URes)
-%!      is det.
+
+%!       experiment_filtering(+T,+Ls,+Us,+LPos,+LNeg,+UPos,+UNeg,+LRes,+URes)
+%!       is det.
 %
 %       Run an experiment separating unlabelled examples of two targets.
 %
@@ -1135,6 +1138,8 @@ generate_examples(Sign,Ts,Es):-
 %       Also see filter_by_language/3 for examples of calling
 %       generate_initial/2 with a filtering term.
 %
+%       @deprecated generate_initial/3
+%
 generate_initial(T/F,Es_f):-
         !
         ,generate_initial(T,Es)
@@ -1155,6 +1160,103 @@ generate_initial(Ts,Es):-
         ,flatten(Es_,Es).
 
 
+
+%!      generate_initial(+Targets,+Symbol,-Examples) is det.
+%
+%       Generate atoms used as examples for a set of targets.
+%
+%       Similar to generate_initial/5 but allows atoms to be generated
+%       from a compisition of two or more target languages.
+%
+%       Targets is either a list of language generation specification
+%       terms S(N,J,K), or a single such term. Targets may also be a
+%       pair Targets/Filter.
+%
+%       In each term S(N,J,K), S is the start symbol of a target
+%       language that must be defined as a Definite Clause Grammar in
+%       test_harness.
+%
+%       Symbol is a predicate indicator, Symbol/Arity, of the atoms in
+%       Examples. Symbol is a single term, therefore all atoms in
+%       Examples will have the same symbol, regardless of the target
+%       language.
+%
+%       N, J, K, are the numbers of atoms to generate and the minimum
+%       (J) and maximum (K) length of strings represented by those
+%       atoms. N can be the atom "all", in which case, you guessed it,
+%       _all_ examples of strings of S of length between J and K will be
+%       generated.
+%
+%       If Targets is a term Targets/Filter, Filter is an atom, the
+%       symbol, but not arity, of a language by which to filter
+%       Examples.
+%
+%       Examples is the list of atoms generated that way. Those are
+%       atoms of each language in Targets, therefore they are atoms in
+%       Definite Clause Grammars notation i.e. they are of the form
+%       S(Xs,Ys,...) where Xs, Ys, etc. are lists of characters
+%       representing strings in a language, the arity of each atom is
+%       the arity in Symbol and and S is the functor in Symbol.
+%
+%       If a Filter option is given all examples of the filtering
+%       language are removed from Examples before returning.
+%
+%       Example:
+%       ==
+%       ?- generate_initial(anbn(all,0,12),S/2,_Es), maplist(writeln,_Es).
+%       s([a,b],[])
+%       s([a,a,b,b],[])
+%       s([a,a,a,b,b,b],[])
+%       s([a,a,a,a,b,b,b,b],[])
+%       s([a,a,a,a,a,b,b,b,b,b],[])
+%       s([a,a,a,a,a,a,b,b,b,b,b,b],[])
+%       true.
+%
+%       ?- generate_initial([anbn(all,0,6),anbm(all,0,3)],S/2,_Es)
+%       ,maplist(writeln,_Es).
+%
+%       s([a,b],[])
+%       s([a,a,b,b],[])
+%       s([a,a,a,b,b,b],[])
+%       s([],[])
+%       s([a],[])
+%       s([a,a],[])
+%       s([a,b],[])
+%       s([a,a,a],[])
+%       s([a,a,b],[])
+%       s([a,a,b],[])
+%       true.
+%       ==
+%
+%       This predicate calls generate_initial/6 and passes it the name
+%       of each tagret language, the Symbol of each atom, and the
+%       corresponding number, and min and max length of strings to
+%       generate.
+%
+%       Also see filter_by_language/3 for examples of calling
+%       generate_initial/3 with a filtering term.
+%
+generate_initial(T/F,S,Es_f):-
+        !
+        ,generate_initial(T,S,Es)
+        ,filter_by_language(F,Es,Es_f).
+generate_initial(T,S,Es):-
+        \+ is_list(T)
+        ,compound(T)
+        ,T =.. [L,N,J,K]
+        ,generate_initial(L,S,N,J,K,Es)
+        ,!.
+generate_initial(Ts,S,Es):-
+        is_list(Ts)
+        ,findall(Es_s
+               ,(member(T,Ts)
+                ,generate_initial(T,S,Es_s)
+                )
+               ,Es_)
+        ,flatten(Es_,Es).
+
+
+
 %!      filter_by_language(+Filter,+Unlabelled,-Filtered) is det.
 %
 %       Filter, or not, Unlabelled examples by a target predicate.
@@ -1173,7 +1275,8 @@ generate_initial(Ts,Es):-
 %       ==
 %       % No filtering
 %       ?- test_harness:generate_initial(anbm(all,0,4),_Es)
-%       , maplist(writeln,_Es), length(_Es,N).s([],[])
+%       , maplist(writeln,_Es), length(_Es,N).
+%       s([],[])
 %       s([a],[])
 %       s([a,a],[])
 %       s([a,b],[])
@@ -1232,6 +1335,66 @@ filter_by_language(S,Us,Us_f):-
 
 
 
+%!      generate_initial(+Language,+Symbol,+N,+Min,+Max,-Atoms) is det.
+%
+%       Generate a set of atoms to use as initial examples of Language.
+%
+%       Language is the symbol, but not arity, of the target theory used
+%       to generate atoms.
+%
+%       Symbol is the predicate indicator, Functor/Arity, of the Atoms
+%       to generate.
+%
+%       N is either a number or the atom all. If N is a number, it's the
+%       number of atoms to generate. If it is "all", then all atoms with
+%       strings of length between Min and Max will be generated.
+%
+%       Min and Max are the upper and lower bounds on the length of
+%       strings, represented as definite clause grammars input lists, in
+%       the generated atoms.
+%
+%       Atoms is the list of generated atoms.
+%
+%       When N is a number this predicate first generates _all_ atoms
+%       with input lists of length between Min and Max, and then samples
+%       N of those atoms, with a call to k_list_samples/3. The sampled
+%       atoms are returned in Atoms.
+%
+generate_initial(L,S,N,J,K,Es):-
+        debug(generate_initial,'Generating ~w ~w examples of length in [~w,~w].'
+             ,[N,L,J,K])
+        ,findall(E
+               ,(between(J,K,I)
+                ,generate_example(L,S,I,E)
+                )
+               ,Es_)
+        ,(   number(N)
+         ->  k_list_samples(N,Es_,Es)
+         ;   N == all
+         ->  Es = Es_
+         ).
+
+
+%!      generate_example(+Target,+Symbol,+Length,-Atom) is nondet.
+%
+%       Generate an Atom with a list of characters of the given Length.
+%
+%       Business end of generate_initial/6,
+%
+generate_example(L,S/2,N,E_):-
+        !
+        ,length(Xs,N)
+        ,E =.. [L,Xs,[]]
+        ,call(E)
+        ,E_ =.. [S,Xs,[]].
+generate_example(L,S/3,N,E_):-
+        length(Is,N)
+        ,E =.. [L,Is,Os,[]]
+        ,call(E)
+        ,E_ =.. [S,Is,Os,[]].
+
+
+
 %!      generate_initial(+Language,+N,+Min,+Max,-Atoms) is det.
 %
 %       Generate a set of atoms to use as initial examples in Poker.
@@ -1253,6 +1416,8 @@ filter_by_language(S,Us,Us_f):-
 %       with input lists of length between Min and Max, and then samples
 %       N of those atoms, with a call to k_list_samples/3. The sampled
 %       atoms are returned in Atoms.
+%
+%       @deprecated generate_initial/6
 %
 generate_initial(S,N,J,K,Es):-
         debug(generate_initial,'Generating ~w ~w examples of length in [~w,~w].'
@@ -1380,7 +1545,7 @@ internal_symbol_(not_palindrome_bin,p).
 
 
 
-%!      count_initial(+Spec,-Count) is det.
+%!      count_initial(+Spec,+Symbol,-Count) is det.
 %
 %       Count the number of examples generated by a Specification.
 %
@@ -1390,15 +1555,18 @@ internal_symbol_(not_palindrome_bin,p).
 %       Spec is a language generation specification term, or a list
 %       thereof.
 %
-%       Count is an integer the number of examples generated by
-%       generate_initial/2 when given Spec.
+%       Symbol is a predicate indicator, Functor/Arity, of the generated
+%       examples.
 %
-count_initial(S,N):-
-        generate_initial(S,Es)
+%       Count is an integer the number of examples generated by
+%       generate_initial/3 when given Spec.
+%
+count_initial(L,S,N):-
+        generate_initial(L,S,Es)
         ,length(Es,N).
 
 
-%!      count_initial(+Lang,+N,+Min,+Max,-Examples) is det.
+%!      count_initial(+Lang,+Symbol,+N,+Min,+Max,-Examples) is det.
 %
 %       Count the number of examples generated given the arguments.
 %
@@ -1408,6 +1576,9 @@ count_initial(S,N):-
 %       Language is the symbol, but not arity, of the target theory used
 %       to generate atoms.
 %
+%       Symbol is a predicate indicator, Functor/Arity, of the generated
+%       examples.
+%
 %       N is either a number or the atom all. If N is a number, it's the
 %       number of atoms to generate. If it is "all", then all atoms with
 %       strings of length between Min and Max will be generated.
@@ -1416,10 +1587,10 @@ count_initial(S,N):-
 %       strings, represented as definite clause grammars input lists, in
 %       the generated atoms.
 %
-%       Atoms is the list of generated atoms.
+%       Examples is the list of generated atoms.
 %
-count_initial(S,N,J,K,M):-
-        generate_initial(S,N,J,K,Es)
+count_initial(L,S,N,J,K,M):-
+        generate_initial(L,S,N,J,K,Es)
         ,length(Es,M).
 
 
